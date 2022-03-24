06e3858e9b243fb87062da68e2467517d53b75d3
Camilo López <camiloj.lopez79@gmail.com>
2022-03-24 17:17:17 -0300

    Se termina de implementar los estilos

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -6,6 +6,26 @@ export const NoteScreen = () => {
   return (
     <div className='notes__main-content'>
       <NotesAppBar /> 
+
+      <div className='notes__content'>
+        <input 
+          type='text'
+          placeholder='Some awesome title'
+          className='notes__title-input'
+          autoComplete='off'
+        />
+        <textarea
+          placeholder='What happened today?'
+          className='notes__textarea'
+        ></textarea>
+
+        <div className='notes__image'>
+          <img 
+            src='https://c4.wallpaperflare.com/wallpaper/108/140/869/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-thumb.jpg'
+            alt='image'
+          />
+        </div>
+      </div>
     </div>
   )
 }
